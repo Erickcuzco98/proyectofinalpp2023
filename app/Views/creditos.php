955b9f8189f60603377926119d7db8c2ed97ab48
Rene Boch <rene.boch.201405814@gmail.com>
2023-10-29 20:34:42 -0600

    Agregando contenido a creditos

--- app/Views/creditos.php
+++ app/Views/creditos.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -47,6 +48,7 @@
         }
     </style>
 </head>
+
 <body>
     <div class="container">
         <h1 class="mt-4"><i class="fas fa-user"></i> Créditos</h1>
@@ -54,13 +56,17 @@
         <p class="creditos">Desarrollado por:</p>
         <ul>
             <li>Erick Rodolfo Cuzco Tunche (Carnet: 202010383)</li>
+            <li>PROCESOS TRABAJADOS: CRUD BIBLIOTECARIO, LIBROS, AUTORES, CATEGORIA.</li>
+            <br>
             <li>René Boch Pirir (Carnet: 201405814)</li>
+            <li>PROCESOS TRABAJADOS: LOGIN DE USUARIOS, DASHBOARD PARA CADA TIPO DE USUARIO, CRUD ADMINISTRADOR, USUARIOS, LIBROS, PRESTAMOS.</li>
         </ul>
-
+        <br>
         <p>Este proyecto es parte de un sistema de registro de biblioteca escolar desarrollado como proyecto académico.</p>
         <a href="./" class="btn btn-regresar">Regresar</a>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/33337d547b.js" crossorigin="anonymous"></script>
 </body>
+
 </html>
